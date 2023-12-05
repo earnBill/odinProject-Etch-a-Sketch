094fd3f253e1132a6413ac7c123e1287940489cf
billTas <billtas@gmail.com>
2023-12-05 20:37:42 +0200

    Create board function

--- code.js
+++ code.js
@@ -1,9 +1,33 @@
+//when load the whole page
+document.addEventListener('DOMContentLoaded', () => {
+    createBoard(16);
+    console.log('hi');
+})
+
+function createBoard(size) {
+    let board = document.querySelector('.board');
+    
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`; 
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let numDivs = size * size;
+
+    for (let i = 0; i < numDivs; i++) {
+        let div = document.createElement('div');
+        board.style.backgroundColor = "yellow"
+        board.insertAdjacentElement('beforeend', div);
+    }
+}
+
+
+
+
 
 // document.addEventListener('DOMContentLoaded', () => { 
 //     console.log('hi')
 
 //     let div = document.createElement('div');
-//     let container = document.querySelector('.container');
+//     let container = document.querySelector('.board');
 
 //     for (let i = 0; i < 16; i++) {
 //         for (let j = 0; j < 16; j++) {
