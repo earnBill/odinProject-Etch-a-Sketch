8a1d4d7a6b2ca0ea9bdbc9d8368faa6c82086e87
billTas <billtas@gmail.com>
2023-12-05 19:49:05 +0200

    Styling the page

---
+++ code.js
@@ -0,0 +1,15 @@
+
+// document.addEventListener('DOMContentLoaded', () => { 
+//     console.log('hi')
+
+//     let div = document.createElement('div');
+//     let container = document.querySelector('.container');
+
+//     for (let i = 0; i < 16; i++) {
+//         for (let j = 0; j < 16; j++) {
+//             div = document.createElement('div');
+//             container.append(div);
+//         }
+//     }
+
+// });
